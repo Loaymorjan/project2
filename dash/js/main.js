a4f63280d53fab5b0b527cbabe63c53d0d64aa23
Loaymorjan <75802707+Loaymorjan@users.noreply.github.com>
2023-10-05 16:50:07 +0300

    Add files via upload

---
+++ dash/js/main.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20
+    }
+  }
+});
